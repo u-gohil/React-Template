6a7f5d4dc3fc6dff937f4e64d7cdd4594129c402
u-gohil <756D6D61@gmail.com>
2020-11-20 23:53:19 +0000

    Webpack configured

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const heading = document.createElement('h1');
+heading.textContent('Ooh webpack!');
+
+const app = document.querySelector('#root');
+app.append(heading);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpacl = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'running-app',
+      //input file
+      template: path.resolve(__dirname, './src/template.html'),
+      //output file
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // CSS
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
